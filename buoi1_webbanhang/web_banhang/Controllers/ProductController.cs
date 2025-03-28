5a77b7e6767791bd95d621674f5f63ad78b546b8
levanhieu <lehieupro1542004@gmail.com>
2025-03-28 13:44:38 +0700

    Asp.net

---
+++ buoi1_webbanhang/web_banhang/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using web_banhang.connect;
+
+namespace web_banhang.Controllers
+{
+    public class ProductController : Controller
+    {
+        private web_banhangEntities _dbContext = new web_banhangEntities();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            var products = _dbContext.products.ToList();
+
+            if (!products.Any())
+            {
+                ViewBag.Message = "Không có sản phẩm nào!";
+            }
+
+            return View(products);
+        }
+    }
+}
---
+++ buoi1_webbanhang/web_banhang/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="web_banhang.MvcApplication" Language="C#" %>
